fe32c0f921385a0ba831de24aead073eb5d492e1
roshnianuragi <anuragiroshni06@gmail.com>
2026-01-14 17:28:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+ 
+import jsonServer from 'json-server';
+import dotenv from 'dotenv';
+dotenv.config();
+ 
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+ 
+const port = process.env.PORT||3002;
+ 
+server.use(middlewares);
+server.use(router);
+ 
+server.listen(port, () => {
+  console.log(`JSON Server is running ${port}`)
+});
+ 
